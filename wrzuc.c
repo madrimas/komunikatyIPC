b0cec239614e956c4c8cae93083fcc15144e6c23
madrimas <zapiorkowski@gmail.com>
2016-05-29 22:09:31 +0200

    Add files via upload

--- wrzuc.c
+++ wrzuc.c
@@ -2,26 +2,25 @@
 #include <stdlib.h>
 #include <sys/msg.h>
 
-struct temp
-{
+struct temp{
     long typ;
     char zawartosc[69];
 };
 
-int main(int argc, char* argv[])
-{
-	key_t klucz;
-	int rozmiar, ID_wiadomosc;
-	struct temp wiadomosc;
+int main(int argc, char* argv[]){
 	
-	if ((klucz = ftok("./wrzuc", 69)) == -1)
-	{
+	key_t klucz;
+	int rozmiar, ID_wiadomosc, send;
+	struct temp wiadomosc;
+
+	klucz = ftok("./wrzuc", 69);
+	if (klucz == -1){
 		perror("ftok nie podolal");
 		exit(1);
 	}
-	
-	if ((ID_wiadomosc = msgget(klucz, 0666 | IPC_CREAT)) == -1)
-	{
+
+	ID_wiadomosc = msgget(klucz, 0666 | IPC_CREAT);		
+	if (ID_wiadomosc == -1){
 		perror("mssget nie podolal");
 		exit(1);
 	}
@@ -31,8 +30,8 @@ int main(int argc, char* argv[])
 	scanf("%s", wiadomosc.zawartosc);
 	rozmiar = sizeof wiadomosc.zawartosc;
 	
-	if ((msgsnd(ID_wiadomosc, &wiadomosc, rozmiar, IPC_NOWAIT)) == -1)
-	{
+	send = msgsnd(ID_wiadomosc, &wiadomosc, rozmiar, IPC_NOWAIT);
+	if (send == -1){
 		perror("msgsnd nie wyslal wiadomosci");
 		exit(1);
 	}
--- wyrzuc.c
+++ wyrzuc.c
@@ -3,72 +3,70 @@
 #include <sys/msg.h>
 #include <time.h>
 
-struct temp
-{
+struct temp{
     long typ;
     char zawartosc[69];
 };
 
-int main(int argc, char* argv[])
-{
-	key_t klucz;
-	int rozmiar, ID_wiadomosc;
-	struct temp wiadomosc;
+int main(int argc, char* argv[]){
 	
+	key_t klucz;
+	int rozmiar, ID_wiadomosc, receive;
+	struct temp wiadomosc;
 	time_t weekend;
     struct tm * test;
+    
     time(&weekend);
     test=localtime(&weekend);
     
-    if ( (test->tm_wday == 0) || (test->tm_wday == 5) || (test->tm_wday == 6) )
-    {
-		if ( (klucz = ftok("./wrzuc", 69)) == -1 )
-		{
+    if ( (test->tm_wday == 0) || (test->tm_wday == 5) || (test->tm_wday == 6) ){
+		klucz = ftok("./wrzuc", 69);
+		
+		if ( klucz == -1 ){
 			perror("ftok nie podlal");
 			exit(1);
-		}	
-		if ( (ID_wiadomosc = msgget(klucz, 0666 | IPC_CREAT)) == -1 )
-		{
+		}
+			
+		ID_wiadomosc = msgget(klucz, 0666 | IPC_CREAT);
+		if ( ID_wiadomosc == -1 ){
 			perror("mssget nie podolal");
 			exit(1);
 		}
+		
 		rozmiar = sizeof wiadomosc.zawartosc;
-		if ( (msgrcv(ID_wiadomosc, &wiadomosc, rozmiar, 1, IPC_NOWAIT)) == -1 )
-		{
+		receive =  msgrcv(ID_wiadomosc, &wiadomosc, rozmiar, 1, IPC_NOWAIT);
+		if ( receive == -1 ){
 			perror("msgrcv nie odebral wiadomosci");
 			exit(1);
 		}
+		
 		int bool = 0;
-		if ( ( ( (int)wiadomosc.zawartosc[0] > 64 && (int)wiadomosc.zawartosc[0] < 91) || ( (int)wiadomosc.zawartosc[0] > 96 && (int)wiadomosc.zawartosc[0] < 123) ) )
-		{
+		if ( ( ( (int)wiadomosc.zawartosc[0] > 64 && (int)wiadomosc.zawartosc[0] < 91) || ( (int)wiadomosc.zawartosc[0] > 96 && (int)wiadomosc.zawartosc[0] < 123) ) ){
 			bool = 1;
 		}
-		if (bool)
-		{
+		
+		if (bool){
 			int i = 0;
-			while (wiadomosc.zawartosc[i] != '\0')
-			{
-				if ( ( (int)wiadomosc.zawartosc[i] > 33 && (int)wiadomosc.zawartosc[i] < 127 ) )
-				{
+			while (wiadomosc.zawartosc[i] != '\0'){
+				if ( ( (int)wiadomosc.zawartosc[i] > 33 && (int)wiadomosc.zawartosc[i] < 127 ) ){
 					bool = 1;
 				}
-				else
-				{
+				else{
 					bool = 0;
 				}
 					i=i+1;
 			}
 		}
 		
-		if (bool)
-		{
+		if (bool){
 			printf("Wiadomosc z kolejki: %s\n", wiadomosc.zawartosc);
 		}
-		else
+		else{
 			printf("Przeslana wiadomosc nie jest prawidlowa.\n");
+		}
 	}
-	else
+	else{
 		printf("Nie ma weekendu a wiec nie moge podac wiadomosci z kolejki.\n");
-	
+	}
 	return 0;
 }
